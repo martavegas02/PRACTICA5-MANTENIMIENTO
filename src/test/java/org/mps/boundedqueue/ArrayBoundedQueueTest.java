1c80cd86deaa10a188204ff25588cc17d522a435
martavegas02 <martitavegas286@gmail.com>
2025-04-24 11:24:42 +0200

    Test comentados

---
+++ src/test/java/org/mps/boundedqueue/ArrayBoundedQueueTest.java
@@ -0,0 +1,48 @@
+package org.mps.boundedqueue;
+
+public class ArrayBoundedQueueTest {
+    
+
+    /*
+     * Constructor
+     * 1. Comprobar que el constructor lanza una excepción si la capacidad es negativa o cero.
+     * 2. Comprobar que el constructor crea un objeto ArrayBoundedQueue con la capacidad correcta.
+     * 3. Comprobar que el constructor crea un objeto ArrayBoundedQueue vacío.
+     */
+
+
+    /*   put
+     * 1. Comprobar que el método put lanza una excepción si la cola está llena.
+     * 2. Comprobar que el método put lanza una excepción si el elemento es nulo.
+     * 3. Comprobar que el método put añade un elemento a la cola y actualiza el tamaño correctamente.
+     */ 
+
+    /*   get
+        * 1. Comprobar que el método get lanza una excepción si la cola está vacía.
+        * 2. Comprobar que el método get devuelve el primer elemento de la cola y actualiza el tamaño correctamente.
+    */
+
+    /*   advance 
+     * 1. Comprobar que el método advance devuelve el índice correcto del siguiente elemento en la cola.
+     */
+
+    /*   isFull
+     * 1. Comprobar que el método isFull devuelve true si la cola está llena.
+     * 2. Comprobar que el método isFull devuelve false si la cola no está llena.
+     */
+
+    /*   isEmpty
+        * 1. Comprobar que el método isEmpty devuelve true si la cola está vacía.
+        * 2. Comprobar que el método isEmpty devuelve false si la cola no está vacía.
+    */
+
+    /*  getFirst
+        * 1. Comprobar que el método getFirst devuelve el índice del primer elemento de la cola.
+     */
+
+    /*  getLast
+     * 1. Comprobar que el método getLast devuelve el índice del último elemento de la cola.
+     */
+
+    
+}
